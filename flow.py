47da2c48ebc84856ad2a3699ab3e00ea0d91b55b
Luca Barioglio <luca.barioglio@cern.ch>
2024-02-19 16:45:46 +0100

    Extend measure to different EP estimators

--- flow.py
+++ flow.py
@@ -35,15 +35,11 @@ pt_axis = hSpFT0AvsNsigmaHe3VsPtvsCent.GetAxis(2)
 n_pt_bins = pt_axis.GetNbins()
 
 for i_pt in range(1, n_pt_bins + 1):
+
     pt_dir = output_file.mkdir(f'pt_{i_pt}')
     pt_dir.cd()
 
-    hSpVsNsigma, hNsigma = utils.getHistos1D(
-        hSpFT0CvsNsigmaHe3VsPtvsCent, i_pt, i_pt, 30, 50, f'hNsigma_{i_pt}', f'hSpVsNsigma_{i_pt}', n_rebin=4)
-    utils.setHistStyle(hNsigma, ROOT.kRed+1, linewidth=2)
-    utils.setHistStyle(hSpVsNsigma, ROOT.kAzure+1, linewidth=2)
-    hSpVsNsigma.GetYaxis().SetRangeUser(-1., 1.)
-
+    # create info panel only once
     info_panel = ROOT.TPaveText(0.7, 0.6, 0.9, 0.82, 'NDC')
     info_panel.SetBorderSize(0)
     info_panel.SetFillStyle(0)
@@ -56,9 +52,48 @@ for i_pt in range(1, n_pt_bins + 1):
         f'{pt_axis.GetBinUpEdge(i_pt)}' + r' GeV/#it{c}'
     info_panel.AddText(pt_label)
 
-    canvas = utils.geCanvasWithTwoPanels(
-        f'cSpVsNsigma_{i_pt}', hSpVsNsigma, hNsigma, info_panel)
+    # FT0C
+    hSpVsNsigma_FT0C, hNsigma_FT0C = utils.getHistos1D(
+        hSpFT0CvsNsigmaHe3VsPtvsCent, i_pt, i_pt, 30, 50, f'hNsigma_{i_pt}_FT0C', f'hSpVsNsigma_{i_pt}_FT0C', n_rebin=4)
+    utils.setHistStyle(hNsigma_FT0C, ROOT.kRed+1, linewidth=2)
+    utils.setHistStyle(hSpVsNsigma_FT0C, ROOT.kAzure+1, linewidth=2)
+    hSpVsNsigma_FT0C.GetYaxis().SetRangeUser(-1., 1.)
 
-    hSpVsNsigma.Write()
-    hNsigma.Write()
-    canvas.Write()
+    canvas_FT0C = utils.geCanvasWithTwoPanels(
+        f'cSpVsNsigma_{i_pt}_FT0C', hSpVsNsigma_FT0C, hNsigma_FT0C, info_panel)
+
+    hSpVsNsigma_FT0C.Write()
+    hNsigma_FT0C.Write()
+    canvas_FT0C.Write()
+
+    # FT0A
+    hSpVsNsigma_FT0A, hNsigma_FT0A = utils.getHistos1D(
+        hSpFT0AvsNsigmaHe3VsPtvsCent, i_pt, i_pt, 30, 50, f'hNsigma_{i_pt}_FT0A', f'hSpVsNsigma_{i_pt}_FT0A', n_rebin=4)
+    utils.setHistStyle(hNsigma_FT0A, ROOT.kRed+1, linewidth=2)
+    utils.setHistStyle(hSpVsNsigma_FT0A, ROOT.kAzure+1, linewidth=2)
+    hSpVsNsigma_FT0A.GetYaxis().SetRangeUser(-1., 1.)
+
+    info_panel_bis = info_panel.Clone('panel_ft0a')
+
+    canvas_FT0A = utils.geCanvasWithTwoPanels(
+        f'cSpVsNsigma_{i_pt}_FT0A', hSpVsNsigma_FT0A, hNsigma_FT0A, info_panel_bis)
+
+    hSpVsNsigma_FT0A.Write()
+    hNsigma_FT0A.Write()
+    canvas_FT0A.Write()
+
+    # FV0A
+    hSpVsNsigma_FV0A, hNsigma_FV0A = utils.getHistos1D(
+        hSpFV0AvsNsigmaHe3VsPtvsCent, i_pt, i_pt, 30, 50, f'hNsigma_{i_pt}_FV0A', f'hSpVsNsigma_{i_pt}_FV0A', n_rebin=4)
+    utils.setHistStyle(hNsigma_FV0A, ROOT.kRed+1, linewidth=2)
+    utils.setHistStyle(hSpVsNsigma_FV0A, ROOT.kAzure+1, linewidth=2)
+    hSpVsNsigma_FV0A.GetYaxis().SetRangeUser(-1., 1.)
+
+    info_panel_ter = info_panel.Clone('panel_fv0a')
+    canvas_FV0A = utils.geCanvasWithTwoPanels(
+        f'cSpVsNsigma_{i_pt}_FV0A', hSpVsNsigma_FV0A, hNsigma_FV0A, info_panel_ter)
+
+    hSpVsNsigma_FV0A.Write()
+    hNsigma_FV0A.Write()
+    canvas_FV0A.Write()
+

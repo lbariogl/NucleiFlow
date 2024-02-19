f291e938af83728fd2c02d501af9ebce5d691412
Luca Barioglio <luca.barioglio@cern.ch>
2024-02-19 12:56:24 +0100

    Update configutsion.json + small fixes

--- flow.py
+++ flow.py
@@ -5,9 +5,12 @@ sys.path.append('utils')
 import utils as utils
 
 
-input_file_name = './run_task/AnalysisResults.root'
+input_file_name = './run_task/AnalysisResults_small.root'
 input_dir_name = 'nucleiFlow'
 output_file_name = 'output.root'
+
+cent_detector_label = 'FT0C'
+cent_limits = [30, 50]
 
 input_file = ROOT.TFile(input_file_name)
 output_file = ROOT.TFile(output_file_name, 'RECREATE')
@@ -36,9 +39,10 @@ for i_pt in range(1, n_pt_bins + 1):
     pt_dir.cd()
 
     hSpVsNsigma, hNsigma = utils.getHistos1D(
-        hSpFT0CvsNsigmaHe3VsPtvsCent, i_pt, i_pt, 30, 50, f'hNsigma_{i_pt}', f'hSpVsNsigma_{i_pt}')
+        hSpFT0CvsNsigmaHe3VsPtvsCent, i_pt, i_pt, 30, 50, f'hNsigma_{i_pt}', f'hSpVsNsigma_{i_pt}', n_rebin=4)
     utils.setHistStyle(hNsigma, ROOT.kRed+1, linewidth=2)
     utils.setHistStyle(hSpVsNsigma, ROOT.kAzure+1, linewidth=2)
+    hSpVsNsigma.GetYaxis().SetRangeUser(-1., 1.)
 
     info_panel = ROOT.TPaveText(0.7, 0.6, 0.9, 0.82, 'NDC')
     info_panel.SetBorderSize(0)
@@ -47,7 +51,7 @@ for i_pt in range(1, n_pt_bins + 1):
     info_panel.SetTextFont(42)
     info_panel.AddText('ALICE')
     info_panel.AddText(r'PbPb, #sqrt{#it{s}_{nn}} = 5.36 TeV')
-    info_panel.AddText(r'30 - 50 % FTOC')
+    info_panel.AddText(f'{cent_limits[0]} - {cent_limits[1]} % {cent_detector_label}')
     pt_label = f'{pt_axis.GetBinLowEdge(i_pt)}' + r' #leq #it{p}_{T} < ' + \
         f'{pt_axis.GetBinUpEdge(i_pt)}' + r' GeV/#it{c}'
     info_panel.AddText(pt_label)
--- utils/utils.py
+++ utils/utils.py
@@ -40,8 +40,8 @@ def getHistos1D(thn_sparse, pt_bin_low, pt_bin_up, cent_low, cent_up, histo1_nam
     hNsigma.SetTitle('')
     hSPvsNsigma_tmp = thn_sparse.Projection(0, 1)
     if n_rebin > 1:
-        hNsigma.Rebin(n_rebin)
-        hSPvsNsigma_tmp.Rebin(n_rebin)
+        hNsigma.RebinX(n_rebin)
+        hSPvsNsigma_tmp.RebinX(n_rebin)
     hSPvsNsigma = getAverage2D(hSPvsNsigma_tmp, histo2_name)
     hSPvsNsigma.SetTitle('')
     return hSPvsNsigma, hNsigma
@@ -49,15 +49,16 @@ def getHistos1D(thn_sparse, pt_bin_low, pt_bin_up, cent_low, cent_up, histo1_nam
 
 def geCanvasWithTwoPanels(canvas_name, histo_1, histo_2, info_panel=None):
     canvas = ROOT.TCanvas(canvas_name, canvas_name, 800, 600)
-    pad_top = ROOT.TPad("pad_top", "pad_top", 0.0, 0.3, 1.0, 1.0, 0)
+    pad_top = ROOT.TPad("pad_top", "pad_top", 0.0, 0.5, 1.0, 1.0, 0)
     pad_top.SetLeftMargin(0.15)
     pad_top.SetBottomMargin(0.)
     pad_top.Draw()
     pad_top.cd()
     histo_1.GetYaxis().SetTitleSize(0.07)
+    histo_1.GetYaxis().SetLabelSize(0.05)
     histo_1.Draw('PE')
     canvas.cd()
-    pad_bottom = ROOT.TPad("pad_bottom", "pad_bottom", 0.0, 0.0, 1.0, 0.3, 0)
+    pad_bottom = ROOT.TPad("pad_bottom", "pad_bottom", 0.0, 0.0, 1.0, 0.5, 0)
     pad_bottom.SetLeftMargin(0.15)
     pad_bottom.SetTopMargin(0.)
     pad_bottom.SetBottomMargin(0.3)
@@ -65,6 +66,8 @@ def geCanvasWithTwoPanels(canvas_name, histo_1, histo_2, info_panel=None):
     pad_bottom.cd()
     histo_2.GetYaxis().SetTitleSize(0.07)
     histo_2.GetYaxis().SetLabelSize(0.05)
+    histo_2.GetXaxis().SetTitleSize(0.07)
+    histo_2.GetXaxis().SetLabelSize(0.05)
     histo_2.Draw('PE')
     if info_panel:
         histo_2.GetListOfFunctions().Add(info_panel)

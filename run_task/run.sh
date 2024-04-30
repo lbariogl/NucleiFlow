b5bd943e34361a5d596c62264129da467a889af6
Luca Barioglio <luca.barioglio@cern.ch>
2024-04-30 09:52:17 +0200

    Update run_task

--- run_task/run.sh
+++ run_task/run.sh
@@ -1,5 +1,12 @@
-o2-analysis-lf-nuclei-flow -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000
-
-
-/data/lbariogl/flow/LHC23_PbPb_pass2_new
-o2-aod-merger --input dir_7/input_data.txt --output AO2D_dir7.root
+o2-analysis-lf-epvector -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 | \
+o2-analysis-pid-tof-base -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 | \
+o2-analysis-ft0-corrected-table -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 | \
+o2-analysis-timestamp -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 | \
+o2-analysis-event-selection -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 | \
+o2-analysis-multiplicity-table -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 | \
+o2-analysis-centrality-table -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 | \
+o2-analysis-lf-nuclei-spectra -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 --aod-writer-keep AOD/NUCLEITABLE/0,AOD/NUCLEITABLEFLOW/0| \
+o2-analysis-lf-flow-qc -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 | \
+o2-analysis-track-propagation -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 | \
+o2-analysis-trackselection -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000 | \
+o2-analysis-qvector-table -b --configuration json://configuration.json --shm-segment-size 25000000000 --aod-memory-rate-limit 25000000000

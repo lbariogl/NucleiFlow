893695041fc290e9b2e6ea7051a0c9e9bfddb3fa
Luca Barioglio <luca.barioglio@cern.ch>
2025-04-16 16:00:57 +0200

    Add v2 plot

--- closure_test/toy_phi_MC.C
+++ closure_test/toy_phi_MC.C
@@ -7,6 +7,7 @@
 #include <TLegend.h>
 #include <TCanvas.h>
 #include <TFile.h>
+#include <TPaveText.h>
 
 double GetPhiInRange(double phi)
 {
@@ -44,6 +45,10 @@ double calcrestmom(TLorentzVector mother, TLorentzVector daug)
   return daug.P();
 }
 
+double calcv2(double phi){
+  return cos(2*phi);
+}
+
 void toy_phi_MC()
 {
   TF1 *f2 = new TF1("f2", "1+2.0*0.5*TMath::Cos(2.0*x)", 0, 2 * TMath::Pi());
@@ -64,6 +69,16 @@ void toy_phi_MC()
   TH1F *hPhiHelium = new TH1F("hPhiHelium", ";#phi (rad);counts", 72, 0.0, 2 * TMath::Pi());
   hPhiHelium->SetMarkerStyle(20);
   hPhiHelium->SetMarkerColor(kBlue);
+
+  TH1F *hV2Proton = new TH1F("hV2Proton", ";cos(2(#phi - #Psi_{2}));counts", 200, -1., 1.);
+  hV2Proton->SetMarkerStyle(20);
+  hV2Proton->SetMarkerColor(kBlack);
+  TH1F *hV2Deuteron = new TH1F("hV2Deuteron", ";cos(2(#phi - #Psi_{2}));counts", 200, -1., 1.);
+  hV2Deuteron->SetMarkerStyle(20);
+  hV2Deuteron->SetMarkerColor(kRed);
+  TH1F *hV2Helium = new TH1F("hV2Helium", ";cos(2(#phi - #Psi_{2}));counts", 200, -1., 1.);
+  hV2Helium->SetMarkerStyle(20);
+  hV2Helium->SetMarkerColor(kBlue);
 
   TLorentzVector mother, deuteron, helium3;
   TLorentzVector d1, d2, d3;
@@ -105,17 +120,22 @@ void toy_phi_MC()
     double p3 = calcrestmom(helium3_vec, d3dummy);
 
     hPhiProton->Fill(GetPhiInRange(d1.Phi()));
+    hV2Proton->Fill(calcv2(d1.Phi()));
 
     if (TMath::Abs(proton1restmom - proton2restmom) < 0.3)
     {
       hPhiDeuteron->Fill(GetPhiInRange(deuteron.Phi()));
+      hV2Deuteron->Fill(calcv2(deuteron.Phi()));
     }
 
     if (TMath::Abs(p1 - p2) < 0.3 && TMath::Abs(p1 - p3) < 0.3 && TMath::Abs(p2 - p3) < 0.3)
     {
       hPhiHelium->Fill(GetPhiInRange(helium3.Phi()));
+      hV2Helium->Fill(calcv2(helium3.Phi()));
     }
   }
+
+  // phi distributions
 
   hPhiProton->Sumw2();
   hPhiDeuteron->Sumw2();
@@ -135,21 +155,62 @@ void toy_phi_MC()
   hPhiDeuteron->Draw("same");
   hPhiHelium->Draw("same");
 
-  TLegend *legPhi = new TLegend(0.21, 0.56, 0.61, 0.86, "", "brNDC");
-  legPhi->SetFillColor(0);
-  legPhi->SetFillStyle(0);
-  legPhi->SetTextSize(0.04);
-  legPhi->SetTextFont(42);
-  legPhi->SetTextColor(1);
-  legPhi->SetBorderSize(0);
+  TLegend *legPhi = new TLegend(0.25, 0.71, 0.38, 0.86, "", "brNDC");
   legPhi->AddEntry(hPhiProton, "proton", "pl");
   legPhi->AddEntry(hPhiDeuteron, "deuteron", "lp");
   legPhi->AddEntry(hPhiHelium, "helium3", "lp");
   legPhi->Draw();
+
+  // v2 distributions
+  hV2Proton->Sumw2();
+  hV2Deuteron->Sumw2();
+  hV2Helium->Sumw2();
+
+  hV2Proton->Scale(1.0 / hV2Proton->Integral());
+  hV2Deuteron->Scale(1.0 / hV2Deuteron->Integral());
+  hV2Helium->Scale(1.0 / hV2Helium->Integral());
+
+  hV2Proton->SetStats(0);
+  hV2Proton->GetXaxis()->SetTitle("#phi (rad)");
+  hV2Proton->GetYaxis()->SetTitle("normalized counts");
+
+  TCanvas *cV2 = new TCanvas("cV2", "v2 distributions", 800, 600);
+  cV2->DrawFrame(-1.01, -0.01, 1.001, 0.05, ";cos(2(#phi - #Psi_{2}));counts");
+  hV2Proton->Draw("same");
+  hV2Deuteron->Draw("same");
+  hV2Helium->Draw("same");
+
+  TLegend *legV2 = new TLegend(0.25, 0.71, 0.38, 0.86, "", "brNDC");
+  legV2->AddEntry(hV2Proton, "proton", "pl");
+  legV2->AddEntry(hV2Deuteron, "deuteron", "lp");
+  legV2->AddEntry(hV2Helium, "helium3", "lp");
+  legV2->Draw();
+
+  // Calculate mean and error for each histogram
+  double meanProton = hV2Proton->GetMean();
+  double errorProton = hV2Proton->GetMeanError();
+  double meanDeuteron = hV2Deuteron->GetMean();
+  double errorDeuteron = hV2Deuteron->GetMeanError();
+  double meanHelium = hV2Helium->GetMean();
+  double errorHelium = hV2Helium->GetMeanError();
+
+  // Add TPaveText to display the mean and error
+  TPaveText *paveV2 = new TPaveText(0.65, 0.65, 0.85, 0.85, "NDC");
+  paveV2->SetFillColor(0);
+  paveV2->SetTextAlign(12);
+  paveV2->SetBorderSize(0);
+  paveV2->AddText(Form("Proton: %.3f #pm %.3f", meanProton, errorProton));
+  paveV2->AddText(Form("Deuteron: %.3f #pm %.3f", meanDeuteron, errorDeuteron));
+  paveV2->AddText(Form("Helium3: %.3f #pm %.3f", meanHelium, errorHelium));
+  paveV2->Draw();
 
   TFile *f = new TFile("toy_phi_MC.root", "recreate");
   hPhiProton->Write();
   hPhiDeuteron->Write();
   hPhiHelium->Write();
   cPhi->Write();
+  hV2Proton->Write();
+  hV2Deuteron->Write();
+  hV2Helium->Write();
+  cV2->Write();
 }

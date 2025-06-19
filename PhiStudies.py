e2dab577616e13309ff10fea70f43a3efd199028
Luca Barioglio <luca.barioglio@cern.ch>
2025-06-19 16:20:41 +0200

    PhyStudies.py takes plots from QC.py

--- PhiStudies.py
+++ PhiStudies.py
@@ -12,6 +12,9 @@ import sys
 sys.path.append("utils")
 import utils as utils
 
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetOptFit(0)
+
 parser = argparse.ArgumentParser(description="Configure the parameters of the script.")
 parser.add_argument(
     "--config",
@@ -29,97 +32,218 @@ config = yaml.full_load(config_file)
 
 input_file_name = config["input_file_name"]
 resolution_file_name = config["resolution_file_name"]
-output_dir_name = config["output_dir_name"]
-
-nuclei_tree_name = config["nuclei_tree_name"]
-ep_tree_name = config["ep_tree_name"]
-
-useSP = True  # config["useSP"]
-
-mandatory_selections = config["mandatory_selections"]
-selection_dict = config["selection_dict"]
-selection_list = selection_dict.values()
-selections = " and ".join(selection_list)
-
-pattern = r"\s*and\s+(abs\(fNsigmaTPC3He\)\s*<\s*(\d+\.?\d*))"
-
-# Search and extract the match without "and"
-match = re.search(pattern, selections)
-nSigmaTPC_selection_string = match.group(1).strip() if match else ""
-n_sigma_selection_from_string = float(match.group(2)) if match else None
-
-selections = re.sub(pattern, "", selections).strip()
-
-# ptdep_selection_dict = config["ptdep_selection_dict"]["fAvgItsClusSizeCosLambda"]
-
-cent_detector_label = config["cent_detector_label"]
-reference_flow_detector = config["reference_flow_detector"]
-resolution_flow_detectors = config["resolution_flow_detectors"]
 
 centrality_classes = config["centrality_classes"]
 pt_bins = config["pt_bins"]
 cent_colours = config["cent_colours"]
 
+# Crea la cartella per i PDF se non esiste
+output_dir = "../results_pass4_new_cent_SP/phi_studies"
+os.makedirs(output_dir, exist_ok=True)
 
 # create output file
-if not os.path.exists(output_dir_name):
-    os.makedirs(output_dir_name)
-output_file = ROOT.TFile(f"{output_dir_name}/phi_studies.root", "recreate")
+output_file = ROOT.TFile(f"{output_dir}/phi_studies.root", "recreate")
+print(f"Output file: {output_file.GetName()}")
 
-# get a unique df from nuclei and ep trees
-nuclei_df = utils.get_df_from_tree(input_file_name, nuclei_tree_name)
+# get resolutions
+res_histo_name = f"Resolution_SP/hResolution_FT0C_FT0A_TPC_SP"
 
-nucleiflow_df = utils.get_df_from_tree(input_file_name, ep_tree_name)
+resolution_file = ROOT.TFile(resolution_file_name, "read")
+hResolution = resolution_file.Get(res_histo_name)
+hResolution.SetDirectory(0)
 
-complete_df = pd.concat([nuclei_df, nucleiflow_df], axis=1, join="inner")
+resolution_v4fromv2_file_name = (
+    "../results_pass4_new_cent_SP/resolution_EP_v2_for_v4.root"
+)
+resolution_v4fromv2_file = ROOT.TFile(resolution_v4fromv2_file_name, "read")
+hResolution_v4fromv2 = resolution_v4fromv2_file.Get(res_histo_name)
+hResolution_v4fromv2.SetDirectory(0)
+utils.setHistStyle(hResolution_v4fromv2, ROOT.kBlue + 2)
 
-# define new columns
-utils.redefineColumns(complete_df, useSP=useSP)
+res_dir = output_file.mkdir("resolutions")
+res_dir.cd()
 
-# apply common selections
-complete_df.query(f"{mandatory_selections} and {selections}", inplace=True)
+cent_limits_tens = [0, 10, 20, 30, 40, 50, 60, 70, 80]
+
+# evaluating true resolutions
+resolutions = []
+hResolution.Write("hResolution_FT0C_FT0A_TPC_SP_standard")
+for i_cent, cent in enumerate(centrality_classes):
+    cent_left_index = cent_limits_tens.index(cent[0])
+    cent_right_index = cent_limits_tens.index(cent[1])
+    print(f"cent_limits_tens[{cent_left_index}]: {cent_limits_tens[cent_left_index]}")
+    print(f"cent_limits_tens[{cent_right_index}]: {cent_limits_tens[cent_right_index]}")
+    if cent_right_index - cent_left_index == 1:
+        resol = hResolution.GetBinContent(cent_left_index + 1)
+        print(f"cent: {cent[0]} - {cent[1]}, resolution: {resol}")
+    else:
+        resol = np.mean(
+            [
+                hResolution.GetBinContent(i)
+                for i in range(cent_left_index + 1, cent_right_index + 1)
+            ]
+        )
+        print(f"cent: {cent[0]} - {cent[1]}, resolution: {resol}")
+    resolutions.append(resol)
+
+# evaluating v4 from v2 resolutions
+resolutions_v4fromv2 = []
+hResolution_v4fromv2.Write("hResolution_FT0C_FT0A_TPC_SP_v4fromv2")
+for i_cent, cent in enumerate(centrality_classes):
+    cent_left_index = cent_limits_tens.index(cent[0])
+    cent_right_index = cent_limits_tens.index(cent[1])
+    print(f"cent_limits_tens[{cent_left_index}]: {cent_limits_tens[cent_left_index]}")
+    print(f"cent_limits_tens[{cent_right_index}]: {cent_limits_tens[cent_right_index]}")
+    if cent_right_index - cent_left_index == 1:
+        resol = hResolution_v4fromv2.GetBinContent(cent_left_index + 1)
+        print(f"cent: {cent[0]} - {cent[1]}, resolution: {resol}")
+    else:
+        resol = np.mean(
+            [
+                hResolution_v4fromv2.GetBinContent(i)
+                for i in range(cent_left_index + 1, cent_right_index + 1)
+            ]
+        )
+        print(f"cent: {cent[0]} - {cent[1]}, resolution: {resol}")
+    resolutions_v4fromv2.append(resol)
+
+
+# get histograms from QC file
+qc_file = ROOT.TFile("../results_pass4_new_cent_SP/qc.root", "read")
+
+# ge v2 histograms from final file
+final_file_name = "../results_pass4_new_cent_SP/final.root"
+final_file = ROOT.TFile(final_file_name, "read")
+
+vV2stat = []
+vV2syst = []
 
 for i_cent, cent in enumerate(centrality_classes):
-    cent_selection = f"fCentFT0C > {cent[0]} and fCentFT0C < {cent[1]}"
-    cent_dir = output_file.mkdir(f"{cent[0]}_{cent[1]}")
-    cent_df = complete_df.query(cent_selection, inplace=False)
-    vPhi_cent = []
-    phi_dir = cent_dir.mkdir("phi")
+    hV2stat = final_file.Get(
+        f"cent_{cent[0]}_{cent[1]}/hV2vsPt_cent_{cent[0]}_{cent[1]}"
+    )
+    hV2stat.SetDirectory(0)
+    vV2stat.append(hV2stat)
+    hV2syst = final_file.Get(
+        f"cent_{cent[0]}_{cent[1]}/hV2vsPt_cent_{cent[0]}_{cent[1]}_syst"
+    )
+    hV2syst.SetDirectory(0)
+    vV2syst.append(hV2syst)
+
+
+# Crea un file ROOT per i canvas
+output_root = ROOT.TFile("fit_phi_psi_canvases.root", "RECREATE")
+
+vV2fromFit = []
+
+for i_cent, cent in enumerate(centrality_classes):
+
+    cent_dir = output_file.mkdir(f"cent_{cent[0]}_{cent[1]}")
 
     pt_bins_arr = np.array(pt_bins[i_cent], dtype=np.float64)
     n_pt_bins = len(pt_bins[i_cent]) - 1
 
+    hV2fromFit = ROOT.TH1F(
+        f"hV2FromFit_cent_{cent[0]}_{cent[1]}",
+        r"; #it{p}_{T} (GeV/#it{c}); v_{2}",
+        n_pt_bins,
+        pt_bins_arr,
+    )
+    utils.setHistStyle(hV2fromFit, cent_colours[i_cent], marker=25)
+
+    vV2fromFit.append(hV2fromFit)
+
     for i_pt in range(0, n_pt_bins):
-        # select the correct pt bin
-        pt_sel = f"abs(fPt) > {pt_bins[i_cent][i_pt]} and abs(fPt) < {pt_bins[i_cent][i_pt+1]}"  # and {ptdep_selection_list[i_pt]}"
-        bin_df = cent_df.query(pt_sel, inplace=False)
 
-        hPhi = ROOT.TH1F(
-            f"hPhi_cent_{cent[0]}_{cent[1]}_pt_{i_pt}", r";#phi; counts", 140, -7.0, 7.0
-        )
-        utils.setHistStyle(hPhi, ROOT.kRed + 2)
-
-        for phi in bin_df["fPhi"]:
-            hPhi.Fill(phi)
-
-        vPhi_cent.append(hPhi)
-        phi_dir.cd()
-        hPhi.Write()
-
-        hPhiMinusPsi_formatted_title = f";phi - #Psi^{{FT0C}}_{{2}}; counts"
-        hPhiMinusPsi = ROOT.TH1F(
-            f"hPhiMinusPsiFT0C_cent_{cent[0]}_{cent[1]}_pt_{i_pt}",
-            hPhiMinusPsi_formatted_title,
-            40,
-            0.0,
-            ROOT.TMath.Pi(),
-        )
-        utils.setHistStyle(hPhiMinusPsi, ROOT.kRed + 2)
-
-        for phi, q, psi, v2 in zip(bin_df["fPhi"], bin_df[f"fPsiFT0C"]):
-            delta_phi = utils.getPhiInRange(phi - psi)
-            sinPhiMinusPsi = ROOT.TMath.Sin(delta_phi)
-            hPhiMinusPsi.Fill(delta_phi)
-
+        hPhiMinusPsi_name = f"{cent[0]}_{cent[1]}/FT0C/hPhiMinusPsi_FT0C_cent_{cent[0]}_{cent[1]}_pt_{i_pt}"
+        print(f"Getting histogram: {hPhiMinusPsi_name}")
+        hPhiMinusPsi = qc_file.Get(hPhiMinusPsi_name)
+        hPhiMinusPsi.SetDirectory(0)
+        hPhiMinusPsi.Rebin(4)
+        utils.setHistStyle(hPhiMinusPsi, ROOT.kBlack)
         cent_dir.cd()
         hPhiMinusPsi.Write()
+
+        # Definisci la funzione di fit con nomi dei parametri
+        fit_func = ROOT.TF1(
+            "fit_func",
+            "[0]*(1+2.0*[1]*TMath::Cos(2.0*x)+2.0*[2]*TMath::Cos(4.0*x))",
+            0,
+            ROOT.TMath.Pi(),
+        )
+        fit_func.SetParName(0, "Norm")
+        fit_func.SetParName(1, "v2raw")
+        fit_func.SetParName(2, "v4raw")
+
+        # Imposta valori iniziali ragionevoli
+        fit_func.SetParameters(hPhiMinusPsi.GetMaximum(), 0.1, 0.01)
+
+        # Esegui il fit
+        hPhiMinusPsi.Fit(fit_func, "R")
+
+        # Parametri e errori
+        norm = fit_func.GetParameter(0)
+        norm_err = fit_func.GetParError(0)
+        v2raw = fit_func.GetParameter(1)
+        v2raw_err = fit_func.GetParError(1)
+        v4raw = fit_func.GetParameter(2)
+        v4raw_err = fit_func.GetParError(2)
+        chi2 = fit_func.GetChisquare()
+        ndf = fit_func.GetNDF()
+
+        c = ROOT.TCanvas(
+            f"cPhiMinusPsiFT0C_{cent[0]}_{cent[1]}_pt{i_pt}",
+            f"cPhiMinusPsiFT0C_{cent[0]}_{cent[1]}_pt{i_pt}",
+            800,
+            600,
+        )
+        hPhiMinusPsi.SetMaximum(1.2 * hPhiMinusPsi.GetMaximum())
+        hPhiMinusPsi.Draw("PE")
+        fit_func.Draw("same")
+
+        # TPaveText per i risultati del fit
+        pave = ROOT.TPaveText(0.37, 0.63, 0.70, 0.87, "NDC")
+        pave.SetFillColor(0)
+        pave.SetBorderSize(0)
+        pave.AddText(r"Fit: Norm * (1 + 2 v_{2}^{raw}cos(2x) + 2 v_{4}^{raw} cos(4x))")
+        pave.AddText(f"Norm = {norm:.3g} #pm {norm_err:.2g}")
+        pave.AddText(f"v2raw = {v2raw:.3g} #pm {v2raw_err:.2g}")
+        pave.AddText(f"v4raw = {v4raw:.3g} #pm {v4raw_err:.2g}")
+        pave.AddText(f"#chi^2/ndf = {chi2:.1f}/{ndf}")
+        pave.Draw()
+
+        # Salva il canvas nel file ROOT
+        c.Write()
+
+        # Salva il canvas come PDF nella cartella
+        pdf_path = os.path.join(output_dir, f"phi_minus_psi_fit_{i_cent}_{i_pt}.pdf")
+        c.SaveAs(pdf_path)
+
+        vV2fromFit[i_cent].SetBinContent(i_pt + 1, v2raw)
+        vV2fromFit[i_cent].SetBinError(i_pt + 1, v2raw_err)
+
+    # vV2fromFit[i_cent].Scale(resolutions[i_cent])
+
+    cent_dir.cd()
+    vV2fromFit[i_cent].Write()
+    vV2stat[i_cent].Write()
+    vV2syst[i_cent].Write()
+
+    cV2fromFit = ROOT.TCanvas(
+        f"cV2FromFit_cent_{cent[0]}_{cent[1]}",
+        f"cV2FromFit_cent_{cent[0]}_{cent[1]}",
+        800,
+        600,
+    )
+    vV2stat[i_cent].Draw("PE")
+    vV2syst[i_cent].Draw("PEsame")
+    vV2fromFit[i_cent].Draw("PEsame")
+
+    legend = ROOT.TLegend(0.69, 0.22, 0.88, 0.30, "", "brNDC")
+    legend.SetBorderSize(0)
+    legend.AddEntry(vV2stat[i_cent], "v_{2} standard", "PF")
+    legend.AddEntry(vV2fromFit[i_cent], "v_{2} from fit", "PE")
+    legend.Draw()
+
+    cV2fromFit.Write()
+    cV2fromFit.SaveAs(f"{output_dir}/cV2FromFit_cent_{cent[0]}_{cent[1]}.pdf")
